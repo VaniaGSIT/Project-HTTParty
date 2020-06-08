c637d356c0f475c19349382d740e467594cc0e2f
Ubuntu <ubuntu@ip-172-31-53-43.ec2.internal>
2020-06-08 19:49:37 +0000

    Update

--- app/models/rubygemsorg.rb
+++ app/models/rubygemsorg.rb
@@ -1,10 +1,10 @@
 class RubyGemsOrg
-include HTTParty
-# GET - /api/v1/gems/[GEM NAME].(json|xml|yaml)
-def self.getGemInfo(name)
-url="https://rubygems.org/api/v1/gems/#{name}.json"
-response = HTTParty.get(url)
-responsebody = JSON.parse(response.body)
-return responsebody
-end
+    include HTTParty
+    # GET - /api/v1/gems/[GEM NAME].(json|xml|yaml)
+    def self.getGemInfo(name)
+        url="https://rubygems.org/api/v1/gems/#{name}.json"
+        response = HTTParty.get(url)
+        responsebody = JSON.parse(response.body)
+        return responsebody
+    end
 end
